96e0b6ee30bb168d0a85d2664c59c5f293f87453
jacoby149 <jackstudent149@gmail.com>
2020-11-17 13:28:05 -0500

    added pie chart code

--- markowitz.py
+++ markowitz.py
@@ -116,6 +116,23 @@ def normal(mu=110,sigma=7.10):
     return plt_to_img(plt)
 
 
+def pie():
+    import matplotlib.pyplot as plt
+
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+    labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+    sizes = [15, 30, 45, 10]
+    explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+    fig1, ax1 = plt.subplots()
+    #ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    return plt_to_img(plt)
+
+
+
 def optimal_portfolio(daily_data):
     n = len(daily_data)
     daily_data = np.asmatrix(daily_data)
