bfdd9bf157ffb204e61ef7499076d2c7599eeeb5
jacoby149 <jackstudent149@gmail.com>
2020-11-17 13:20:03 -0500

    jinja injected latex

--- report.py
+++ report.py
@@ -1,12 +1,18 @@
 
 from latex import build_pdf
+from jinja2 import FileSystemLoader
+from latex.jinja2 import make_env
 
 def make_report():
-    min_latex = (r"\documentclass{article}"
-                r"\begin{document}"
-                r"Hello, world!"
-                r"\end{document}")
 
-    # this builds a pdf-file inside a temporary directory
-    pdf = build_pdf(min_latex)
-    pdf.save_to('pdfs/ex1.pdf')
+    # create a jinja2 environment with latex-compatible markup and instantiate a
+    # template
+    env = make_env(loader=FileSystemLoader('.'))
+    tpl = env.get_template('template.tex')
+
+    # create a greeting for all of our friends
+    for name in ['Alice', 'Bob', 'Carol']:
+        filename = 'pdfs/hello-{}.pdf'.format(name.lower())
+
+        pdf = build_pdf(tpl.render(name=name))
+        pdf.save_to(filename)
