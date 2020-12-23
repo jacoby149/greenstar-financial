d6c7d373be3482b45c86f63f0871c0cb673ab61e
jacoby149 <jackstudent149@gmail.com>
2020-12-23 12:59:04 -0500

    risk simplified out of requests.js

--- static/request.js
+++ static/request.js
@@ -17,12 +17,14 @@ function linkload() {
         input = document.getElementsByName(key)[0];
         input.value = decodeURIComponent(urlParams.get(key));
     }
-    //risk = String(document.getElementById("risk").value);
-
 }
 
 //Make a link with all of the form inputs
 function linkmake() {
+
+    //add risk to the hidden risk input value
+    document.getElementById("risk").value = risk;
+
     inputs = document.getElementById("captable").getElementsByTagName("input");
     const urlParams = new URLSearchParams();
     for (input of inputs) {
@@ -49,20 +51,6 @@ function update_vals() {
     //document.getElementById("weight").innerHTML = "Portfolio : " + portfolio + "%"
 }
 
-function setRisk() {
-    console.log("adjusting risk");
-    risk = $(this).val();
-    console.log("risk : " + risk)
-    console.log(risk + " of " + 100)
-    if (portfolios.length == 0) {
-        console.log("Please load graphs first ");
-        return;
-    }
-    update_vals();
-    //set variables that will post to flask to select risk level
-    //display this risk level on the screen.
-
-}
 
 ////////////////////////////////
 //  NETWORKING JAVASCRIPT  /////
@@ -89,12 +77,6 @@ function graphs() {
     form = {}
     $('#captable').serializeArray().map(function (x) { form[x.name] = x.value })
 
-    //adding risk to the form manually is no longer an issue
-    //console.log(risk)
-    //console.log(document.getElementById("risk").value);
-    //form["risk"] = risk
-    //console.log("FORM :" + form)
-
     document.getElementById("message").innerHTML = "Loading Graphs ...";
     $.post("/load_graphs", form, load_graphs)
         .fail(function (error) {
@@ -115,20 +97,10 @@ function backtest() {
     $.post("/back", { "risk": risk }, load_backtest);
 }
 
-//execution
-var portfolios = []
-var risk = document.getElementById("risk").value;
-var means = []
-var stds = []
-console.log("Default risk is " + risk);
-$("#risk").on("change", setRisk);
-
-
 // Generate Report
 function genReport() {
     if (document.getElementById("mean").innerHTML != "") {
         form = document.getElementById('captable')
-        form.appendChild(document.getElementById('risk'))
         response = form.submit()
         console.log("response submitted")
     }
@@ -136,3 +108,11 @@ function genReport() {
         console.log("Hit Load Graphs to Gen a Report")
     }
 }
+
+//execution
+var portfolios = []
+var risk = document.getElementById("risk").value;
+var means = []
+var stds = []
+console.log("Default risk is " + risk);
+http://localhost:5000/?risk=109&name=Jacob%2520Hoffman&birthday=12%252F28%252F1997&term=7&date-input=2020-12-23&Large+Cap+Growth=200&Large+Cap+GrowthY=0%2525&Large+Cap+GrowthX=100%2525&Large+Cap+Value=200&Large+Cap+ValueY=0%2525&Large+Cap+ValueX=100%2525&Small+Cap+Growth=200&Small+Cap+GrowthY=0%2525&Small+Cap+GrowthX=100%2525&Small+Cap+Value=200&Small+Cap+ValueY=0%2525&Small+Cap+ValueX=100%2525&Mid+Cap=200&Mid+CapY=0%2525&Mid+CapX=100%2525&International+Stock=&International+StockY=0%2525&International+StockX=100%2525&Emerging+Mkt+Stock=&Emerging+Mkt+StockY=0%2525&Emerging+Mkt+StockX=100%2525&International+Gov+Bonds=&International+Gov+BondsY=0%2525&International+Gov+BondsX=100%2525&Long+Gov+Bonds=&Long+Gov+BondsY=0%2525&Long+Gov+BondsX=100%2525&Corporate+Bonds=&Corporate+BondsY=0%2525&Corporate+BondsX=100%2525&High+Yield+Bonds=&High+Yield+BondsY=0%2525&High+Yield+BondsX=100%2525&Municipal+Bonds=&Municipal+BondsY=0%2525&Municipal+BondsX=100%2525&Foreign+Bonds=&Foreign+BondsY=0%2525&Foreign+BondsX=100%2525&Emerging+Mkt+Debt=&Emerging+Mkt+DebtY=0%2525&Emerging+Mkt+DebtX=100%2525&V.C.=&V.C.Y=0%2525&V.C.X=100%2525&Real+Estate=&Real+EstateY=0%2525&Real+EstateX=100%2525&Commodities=&CommoditiesY=0%2525&CommoditiesX=100%2525&Cash=&CashY=0%2525&CashX=100%2525
