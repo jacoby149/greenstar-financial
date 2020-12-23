4c5fd9a2f22b88c6702bd217712874a1c6666a96
jacoby149 <jackstudent149@gmail.com>
2020-12-23 13:03:42 -0500

    fix of request.js code for report generation

--- static/request.js
+++ static/request.js
@@ -41,16 +41,6 @@ function showhide() {
     else cap.style.display = "none";
 }
 
-function update_vals() {
-    std = stds[risk]
-    mean = means[risk]
-    portfolio = portfolios[risk]
-    console.log(portfolios)
-    document.getElementById("std").innerHTML = "Risk: " + Math.round(std * 10000) / 100 + "%"
-    document.getElementById("mean").innerHTML = "Return: " + Math.round(mean * 10000) / 100 + "%"
-    //document.getElementById("weight").innerHTML = "Portfolio : " + portfolio + "%"
-}
-
 
 ////////////////////////////////
 //  NETWORKING JAVASCRIPT  /////
@@ -66,7 +56,6 @@ function load_graphs(data) {
     stds = eval(data["risks"])
     means = eval(data["returns"])
     portfolios = eval(data["portfolios"])
-    update_vals()
     cap = document.getElementById("captable");
     cap.style.display = "none";
 }
@@ -99,7 +88,7 @@ function backtest() {
 
 // Generate Report
 function genReport() {
-    if (document.getElementById("mean").innerHTML != "") {
+    if (document.getElementById("graphs").innerHTML != "") {
         form = document.getElementById('captable')
         response = form.submit()
         console.log("response submitted")
@@ -115,4 +104,3 @@ var risk = document.getElementById("risk").value;
 var means = []
 var stds = []
 console.log("Default risk is " + risk);
-http://localhost:5000/?risk=109&name=Jacob%2520Hoffman&birthday=12%252F28%252F1997&term=7&date-input=2020-12-23&Large+Cap+Growth=200&Large+Cap+GrowthY=0%2525&Large+Cap+GrowthX=100%2525&Large+Cap+Value=200&Large+Cap+ValueY=0%2525&Large+Cap+ValueX=100%2525&Small+Cap+Growth=200&Small+Cap+GrowthY=0%2525&Small+Cap+GrowthX=100%2525&Small+Cap+Value=200&Small+Cap+ValueY=0%2525&Small+Cap+ValueX=100%2525&Mid+Cap=200&Mid+CapY=0%2525&Mid+CapX=100%2525&International+Stock=&International+StockY=0%2525&International+StockX=100%2525&Emerging+Mkt+Stock=&Emerging+Mkt+StockY=0%2525&Emerging+Mkt+StockX=100%2525&International+Gov+Bonds=&International+Gov+BondsY=0%2525&International+Gov+BondsX=100%2525&Long+Gov+Bonds=&Long+Gov+BondsY=0%2525&Long+Gov+BondsX=100%2525&Corporate+Bonds=&Corporate+BondsY=0%2525&Corporate+BondsX=100%2525&High+Yield+Bonds=&High+Yield+BondsY=0%2525&High+Yield+BondsX=100%2525&Municipal+Bonds=&Municipal+BondsY=0%2525&Municipal+BondsX=100%2525&Foreign+Bonds=&Foreign+BondsY=0%2525&Foreign+BondsX=100%2525&Emerging+Mkt+Debt=&Emerging+Mkt+DebtY=0%2525&Emerging+Mkt+DebtX=100%2525&V.C.=&V.C.Y=0%2525&V.C.X=100%2525&Real+Estate=&Real+EstateY=0%2525&Real+EstateX=100%2525&Commodities=&CommoditiesY=0%2525&CommoditiesX=100%2525&Cash=&CashY=0%2525&CashX=100%2525
