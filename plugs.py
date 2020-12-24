412da41a42522bfdb69764c2cdf45cbb91c761df
jacoby149 <jackstudent149@gmail.com>
2020-12-24 12:01:31 -0500

    Revert "merge reqs"
    This reverts commit 928a67b15afb454bf693d5b5801cc3494f041d68.

--- plugs.py
+++ plugs.py
@@ -5,7 +5,7 @@ from mpld3 import plugins
 
 class customHover(plugins.PluginBase):
     """A Plugin to enable an HTML tooltip:
-    formatted text which hovers over points.
+    formated text which hovers over points.
     Parameters
     ----------
     points : matplotlib Collection or Line2D object
@@ -108,6 +108,10 @@ class dHover(plugins.PluginBase):
     
     dHover.prototype.draw = function(){
           var obj = mpld3.get_element(this.props.id);
+          
+          var width = ##########    d3 was already confusing, this mlpd3 makes it so much more foncusing???     #############
+          
+          
           obj.elements()
             .append('rect')
             .style("fill", "none")
